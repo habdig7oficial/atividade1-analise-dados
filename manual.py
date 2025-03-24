d96366799d82d4bc26c181e3eec21480af66bd0a
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-03-24 15:06:34 -0300

    ok

--- manual.py
+++ manual.py
@@ -1,13 +1,14 @@
 from lib.funcs import *
 from math import *
 
-arr = [291, 10.9, 47, 86, 44, 18.9, 1, 50, 190.4, 45.7, 28.5, 18.9, 16, 34, 8.6, 9.6]
-select_sort(arr)
 
-print(f"""
+def print_relatorio(arr: list[float]) -> None:
+    select_sort(arr)
+
+    print(f"""
 Testes Básicos:
 Min: {get_min(arr)}
-Sort: {arr}
+Sort: {arr if len(arr) < 40 else len(arr)}
 
 Média:  {media(arr)}
 Mediana: {mediana(arr)}
@@ -22,4 +23,19 @@ Coeficienciente de Variação Populacional: {sqrt(variancia(arr, is_amostral=Fal
 1° quartil: {mediana(arr[0:int(len(arr)/2)])}
 3° quartil: {mediana(arr[int(len(arr) /2):len(arr)])}
 
-""")
+    """)
+
+arr = [291, 10.9, 47, 86, 44, 18.9, 1, 50, 190.4, 45.7, 28.5, 18.9, 16, 34, 8.6, 9.6]
+
+print_relatorio(arr)
+
+import csv
+import sys
+
+data_arr =[]
+with open("./Video_Games_Sales_as_at_22_Dec_2016.csv") as csvfile:
+	data = csv.DictReader(csvfile, delimiter=",")
+	for i in data:
+		data_arr.append(float(i["Global_Sales"]))
+
+print_relatorio(data_arr)
